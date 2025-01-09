3aee24f30cbba625b260502e884771946fe39971
Asteinch <andreassteinnes@gmail.com>
2025-01-09 12:44:01 +0100

    Put wallet functionality in class

--- src/wallet.py
+++ src/wallet.py
@@ -2,56 +2,83 @@ from ecdsa import SigningKey, VerifyingKey, SECP256k1
 import base58
 import hashlib
 
-def get_address(pk):
+class Wallet():
 
-    sha256_hash = hashlib.sha256(pk).digest()
-    ripemd160 = hashlib.new('ripemd160')
-    ripemd160.update(sha256_hash)
-    public_key_hash = ripemd160.digest()
+    def __init__(self, pk : str = None):
 
-    versioned_payload = b'\x00' + public_key_hash
+        self.sk_raw = None
+        self.pk_raw = None
+        self.address = None
 
-    checksum = hashlib.sha256(hashlib.sha256(versioned_payload).digest()).digest()[:4]
+        self.sk_hex = None
+        self.pk_hex = None
 
-    address_bytes = versioned_payload + checksum
+        self.import_key(pk) if pk != None else self.create_key()
 
-    address = base58.b58encode(address_bytes).decode('utf-8')
-    return address
+    def import_key(self, sk):
 
-def sign_transaction(sk : SigningKey, trx_data):
-
-    byte_data = trx_data.encode('utf-8')  # Encoding data to bytes
-
-    tx_hash = hashlib.sha256(byte_data).digest()
-
-    signature = sk.sign(tx_hash)
-
-    return signature
-
-def verify_transaction(pk : VerifyingKey, trx_data, signature):
-
-    # Will be moves in the futures cause validations dosent happen in wallet.
-
-    byte_data = trx_data.encode('utf-8')  # Encoding data to bytes
-    tx_hash = hashlib.sha256(byte_data).digest()
-
-    try:
-        return pk.verify(signature, tx_hash)
-    except:
-        return False
+        self.sk_raw = SigningKey.from_string(bytes.fromhex(sk), curve=SECP256k1)
+        self.create_wallet(self.sk_raw)
 
 
+    def create_key(self):
 
-raw_sk = SigningKey.generate(curve=SECP256k1)
-raw_pk = raw_sk.get_verifying_key()
+        self.sk_raw = SigningKey.generate(curve=SECP256k1)
+
+        self.create_wallet(self.sk_raw)
+
+    def create_wallet(self, sk : SigningKey):
+
+        self.pk_raw = sk.get_verifying_key()
+        self.address = self.get_address(self.pk_raw.to_string())
+
+        self.sk_hex = self.sk_raw.to_string().hex()
+        self.pk_hex = self.pk_raw.to_string().hex()
+
+    def get_address(self, pk : str):
+
+        sha256_hash = hashlib.sha256(pk).digest()
+        ripemd160 = hashlib.new('ripemd160')
+        ripemd160.update(sha256_hash)
+        public_key_hash = ripemd160.digest()
+
+        versioned_payload = b'\x00' + public_key_hash
+
+        checksum = hashlib.sha256(hashlib.sha256(versioned_payload).digest()).digest()[:4]
+
+        address_bytes = versioned_payload + checksum
+
+        address = base58.b58encode(address_bytes).decode('utf-8')
+        return address
+
+    def sign_transaction(self, sk : SigningKey, trx_data):
+
+        byte_data = trx_data.encode('utf-8')  # Encoding data to bytes
+
+        tx_hash = hashlib.sha256(byte_data).digest()
+
+        signature = sk.sign(tx_hash)
+
+        return signature
+
+    def verify_transaction(self, pk : VerifyingKey, trx_data, signature):
+
+        # Will be moves in the futures cause validations dosent happen in wallet.
+
+        byte_data = trx_data.encode('utf-8')  # Encoding data to bytes
+        tx_hash = hashlib.sha256(byte_data).digest()
+
+        try:
+            return pk.verify(signature, tx_hash)
+        except:
+            return False
 
 
-sk = raw_sk.to_string().hex()
-pk = raw_pk.to_string().hex()
 
-address = get_address(raw_pk.to_string())
+wallet = Wallet()
 
-print("Your private key:", sk)
-print("Your public key:", pk)
-print("Your address:", address)
+
+print("Your private key:", wallet.sk_hex)
+print("Your public key:", wallet.pk_hex)
+print("Your address:", wallet.address)
 
