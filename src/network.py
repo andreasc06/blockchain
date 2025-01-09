366ba5b2ee9cf812fab35f1adbe038c52df2c9b9
Asteinch <andreassteinnes@gmail.com>
2025-01-09 17:53:39 +0100

    a network that reccieves trx broadcast

---
+++ src/network.py
@@ -0,0 +1,18 @@
+from src.block import Block
+# Simulated internett. Just a Object where broadcastet transactions are stored and where nodes can add transaction to mempool
+
+class Network:
+
+    def __init__(self):
+
+        self.broadcasted_trx = []
+        self.broadcasted_blocks = []
+
+    def trx_broadcast(self, trx_data : list):
+
+        self.broadcasted_trx.append(trx_data)
+
+    def block_broadcast(self, block : Block):
+
+        self.broadcasted_blocks.append(block)
+
