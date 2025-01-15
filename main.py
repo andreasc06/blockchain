0798c8c6da7eb158d49c1157d9199debc2d7ccc6
Asteinch <andreassteinnes@gmail.com>
2025-01-15 11:32:34 +0100

    refactor: for new features

--- main.py
+++ main.py
@@ -19,38 +19,50 @@ wallet = Wallet(network, node, "0a9f0176251c8be806dd7980bc80c59d14a30fbf426b3dc5
 
 genesis_input =  [Utxo(100, "COINBASE")]
 genesis_output = [Utxo(50, ADDRESS_A),
-                  Utxo(30, ADDRESS_B), 
+                  Utxo(30, ADDRESS_A), 
                   Utxo(20, ADDRESS_C)]
 
 genesis_transactions = [Trx(genesis_input, genesis_output)]
+
+print("\nMining genesis......")
 
 genesis_block = Block(genesis_transactions, 
                       None, 
                       miner.mine_block(genesis_transactions, None), 
                       0)
 
-
-
 network.broadcasted_blocks.append(genesis_block) 
 node.listen_and_verify_block_broadcast()
 
-time.sleep(2)
-
-node.get_all_utxos()
 wallet.get_utxos()
 
-wallet.send_transaction("1KgBbcrskBsxvaKkN94jL3oNxRj4LDAk3J", 50)
+print("\n UTXO in circulation")
+for utxo in node.utxo_pool:
+
+    print(utxo.amount, utxo.assigned_adr)
+
+print("\nSending 60 to", ADDRESS_B, "form", ADDRESS_A)
+
+wallet.send_transaction(ADDRESS_B, 60, [Utxo(50, wallet.address), Utxo(30, wallet.address)])
 node.listen_and_verify_trx_broadcast()
+network.clear_network()
+
+print("\nMining block......")
 
 next_block = Block(node.mempool,      
                    genesis_block.calculate_block_hash(), 
                    miner.mine_block(node.mempool, genesis_block.calculate_block_hash()), 
                    1)
 
+print("\nBroadcasting block..\n")
 
-network.broadcasted_blocks.append(next_block) 
+network.broadcasted_blocks.append(next_block)   
 node.listen_and_verify_block_broadcast()
+network.clear_network()
+wallet.get_utxos()
 
-print(node.blockchain)
+print("\n UTXO in circulation")
+for utxo in node.utxo_pool:
 
+    print(utxo.amount, utxo.assigned_adr)
 
